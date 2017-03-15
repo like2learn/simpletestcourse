956a5c31c01015f3d2a20b54c9a60063314def3a
fludev <fluentdev@mail.ru>
2017-03-16 05:06:07 +0600

    added task_020

--- src/main/java/com/e16/Main.java
+++ src/main/java/com/e16/Main.java
@@ -1,25 +1,6 @@
 package com.e16;
 
-import com.e16.task_018.Pool;
-
 public class Main {
     public static void main(String[] args) {
-        Pool pool = new Pool(new int[][] {
-                {1, 1, 1},
-                {1, 1, 1},
-                {1, 1, 1}
-        });
-
-
-        System.out.println(pool.maxUnion());
-
-
-        /*int[][] arr = new int[][] {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
-
-        System.out.println(arr[1][2]);*/
     }
 }
